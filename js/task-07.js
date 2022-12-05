81b54c3cd7ffb7d3c06ecb060807932e6ac13269
vsr712 <vsrod712@gmail.com>
2022-12-05 22:15:55 +0100

    Copy

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
+const inputRangeRef = document.querySelector("#font-size-control");
+const inputTextRef = document.querySelector("#text");
 
-const input = document.getElementById("font-size-slider");
-const span = document.getElementById("text");
-input.addEventListener("input", handleInputRange);
-function handleInputRange(event) {
-  span.style.fontSize = event.currentTarget.value + "px";
-}
+const handleInput = () => {
+  inputTextRef.style.fontSize = `${inputRangeRef.value}px`;
+};
+
+inputRangeRef.addEventListener("input", handleInput);
